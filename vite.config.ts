4f2667d19349625941709d61c4b77e2c635e71ae
Andrés Yepes <andresyv93@outlook.es>
2022-04-14 09:42:21 +0200

    root(tests): tests environment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [WindiCSS(), vue()],
+  test: {
+    environment: "jsdom",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
