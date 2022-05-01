bab681076cee072d3840329a9c466182dc1104a0
Andrés Yepes <andresyv93@outlook.es>
2022-05-01 21:01:26 +0200

    FIX(Styles): scoped styles

--- src/components/FAppBar/FAppBar.vue
+++ src/components/FAppBar/FAppBar.vue
@@ -2,7 +2,6 @@
 import type { PropType } from "vue";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { computed } from "vue";
-import "./FAppBar.sass";
 
 type FAppBarVariants = "default" | "primary" | "accent" | "transparent";
 const props = defineProps({
@@ -43,3 +42,22 @@ const classes = computed(() => ({
     </div>
   </nav>
 </template>
+
+<style lang="sass" scoped>
+.f-app-bar
+  @apply px-4 py-3
+.f-app-bar--fixed
+  @apply fixed top-0 inset-x-0 z-50 h-14 flex justify-between items-center
+.f-app-bar-scrolled
+  @apply shadow-md bg-white dark:bg-dark-darkest text-slate-700 dark:text-white
+.f-app-bar--default
+  @apply bg-white text-slate-700
+.f-app-bar--primary
+  @apply bg-primary-300 text-white
+.f-app-bar--accent
+  @apply bg-accent-300 text-white
+.f-app-bar--transparent
+  @apply bg-transparent dark:bg-transparent
+.f-app-bar--dark
+  @apply bg-[#001b29] text-white
+</style>
--- src/components/FButton/FButton.vue
+++ src/components/FButton/FButton.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import type { PropType } from "vue";
-import "./FButton.sass";
 
 type FButtonVariants =
   | "default"
@@ -42,3 +41,36 @@ const classes = computed(() => ({
     <slot />
   </button>
 </template>
+
+<style lang="sass" scoped>
+.f-button
+  @apply rounded text-sm font-base py-1 px-4 uppercase
+  &.f-button--default
+    @apply bg-gray-200 hover:bg-gray-300 shadow dark:bg-dark
+  &.f-button--text
+    @apply bg-transparent shadow-none hover:bg-black hover:bg-opacity-10
+  &.f-button--gradient
+    @apply gradient-to-r hover:bg-primary-300 shadow text-white hover:bg-primary-300
+  &.f-button--primary
+    @apply bg-primary-300 hover:bg-primary-400 shadow text-white
+    &.f-button--outline
+      @apply bg-opacity-10 hover:bg-opacity-20 text-primary-400 shadow-none border border-primary-400
+  &.f-button--warning
+    @apply bg-warning-300 hover:bg-warning-400 shadow text-white
+    &.f-button--outline
+      @apply bg-opacity-10 hover:bg-opacity-20 text-warning-400 shadow-none border border-warning-400
+  &.f-button--error
+    @apply bg-error-300 hover:bg-error-400 shadow text-white
+    &.f-button--outline
+      @apply bg-opacity-10 hover:bg-opacity-20 text-error-400 shadow-none border border-error-400
+  &.f-button--info
+    @apply bg-info-300 hover:bg-info-400 shadow text-white
+    &.f-button--outline
+      @apply bg-opacity-10 hover:bg-opacity-20 text-info-400 shadow-none border border-info-400
+  &.f-button--success
+    @apply bg-success-300 hover:bg-success-400 shadow text-white
+    &.f-button--outline
+      @apply bg-opacity-10 hover:bg-opacity-20 text-success-400 shadow-none border border-success-400
+  &.f-button--rounded
+    @apply rounded-full
+</style>
