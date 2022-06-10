25dadfa826730d2342b125b4b88b5fa68d8c2946
Andrés Yepes <andresyv93@outlook.es>
2022-06-11 01:38:55 +0200

    FEAT(List): new list and list-item components

---
+++ src/components/core/FList/FList.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps({
+  tag: {
+    type: String,
+    default: "ul",
+  },
+});
+</script>
+<template>
+  <component :is="tag" class="f-list">
+    <slot />
+  </component>
+</template>
+<style scoped>
+.f-list {
+  --at-apply: "py-3 px-2 bg-white dark:bg-dark-darkest rounded-md shadow space-y-1";
+}
+</style>
---
+++ src/components/core/FList/FListItem.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { useSlots } from "vue";
+
+defineProps({
+  tag: { type: String, default: "li" },
+});
+const slots = useSlots();
+</script>
+
+<template>
+  <component class="f-list-item" :is="tag">
+    <div v-if="slots.prefix">
+      <slot name="prefix" />
+    </div>
+    <div flex-1 px-2>
+      <slot />
+    </div>
+    <div v-if="slots.suffix" px-3 py-1>
+      <slot name="suffix" />
+    </div>
+  </component>
+</template>
+<style scoped>
+.f-list-item {
+  --at-apply: "flex flex-row items-center bg-white dark:bg-dark-darkest border-b border-b-slate-200 last:border-b-0 p-1";
+}
+</style>
