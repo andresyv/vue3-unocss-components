548d0bf82c0d06c3afbc9d0f8b5e4609dd6494cc
Andrés Yepes <andresyv93@outlook.es>
2022-06-11 01:38:36 +0200

    FEAT(FAvatar): new avatar component

---
+++ src/components/core/FAvatar/FAvatar.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import FImage from "../FImage/FImage.vue";
+
+defineProps({
+  src: { type: String },
+  alt: { type: String, required: true },
+  size: { type: Number, default: 30 },
+  tile: { type: Boolean, default: false },
+});
+</script>
+
+<template>
+  <f-image
+    :src="src ?? '/images/user-placeholder.png'"
+    :alt="alt"
+    :width="size"
+    :height="size"
+    :class="{ shadow: !tile, 'rounded-full': !tile }"
+  />
+</template>
